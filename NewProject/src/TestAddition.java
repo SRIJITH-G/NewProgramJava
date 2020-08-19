461213e7c0eb7945bcec7dea15e0fb7d4466d3e6
SRIJITH-G <srijithgovindarajan@gmail.com>
2020-08-19 17:07:30 +0530

    NewCommit1

---
+++ NewProject/src/TestAddition.java
@@ -0,0 +1,15 @@
+
+public class TestAddition {
+
+	public static void main(String [] args)
+	 
+	{
+		int a=4 , b=5, c = 0;
+		
+		System.out.println("The result is:"+c);
+		c = a + b;
+		
+	}
+}
+
+
